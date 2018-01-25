8ff60d45005deed37a45b0be9ece4d23c36bfdd7
Alexander Gunin <guninalexander@gmail.com>
2018-01-25 21:48:25 +0300

    limit batch

--- models/AcousticModel.py
+++ models/AcousticModel.py
@@ -649,12 +649,12 @@ class AcousticModel(object):
         """
         # Base output is to accumulate loss, error_rate, increase the mini-batchs counter and keep the hidden state for
         # next batch
-        output_feed = [self.acc_mean_loss_op, self.acc_error_rate_op,
-                       self.increase_mini_batch_op, self.rnn_keep_state_op]
+        output_feed = [self.acc_mean_loss_op.op, self.acc_error_rate_op.op,
+                       self.increase_mini_batch_op.op, self.rnn_keep_state_op.op]
 
         if compute_gradients:
             # Add the update operation
-            output_feed.append(self.accumulate_gradients_op)
+            output_feed.append(self.accumulate_gradients_op.op)
             # and feed the dropout layer the keep probability values
             input_feed = {self.input_keep_prob_ph: self.input_keep_prob,
                           self.output_keep_prob_ph: self.output_keep_prob}
@@ -671,7 +671,7 @@ class AcousticModel(object):
         return mini_batch_num
 
     def start_batch(self, session, is_training, run_options=None, run_metadata=None):
-        output = [self.acc_error_rate_zero_op, self.acc_mean_loss_zero_op, self.mini_batch_zero_op]
+        output = [self.acc_error_rate_zero_op.op, self.acc_mean_loss_zero_op.op, self.mini_batch_zero_op.op]
 
         ##self.set_is_training(session, is_training)
         if is_training:
@@ -693,7 +693,8 @@ class AcousticModel(object):
                 output_feed.append(self.rnn_state_zero_op)
 
         # If a tensorboard dir is configured then run the merged_summaries operation
-        if self.tensorboard_dir is not None:
+        NotMe = False
+        if self.tensorboard_dir is not None and NotMe:
             if is_training:
                 output_feed.append(self.train_summaries_op)
             else:
@@ -705,7 +706,7 @@ class AcousticModel(object):
         batchs_count = outputs[2]
         global_step = outputs[3]
 
-        if self.tensorboard_dir is not None:
+        if self.tensorboard_dir is not None and NotMe:
             summary = outputs[-1]
             if self.is_ditributed:
                 if self.is_chief:
