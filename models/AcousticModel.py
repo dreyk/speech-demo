fb14c2610ab65d4b5a450e9fb59e8503b5faa4f2
Alexander Gunin <guninalexander@gmail.com>
2018-03-22 21:07:34 -0700

    slice

--- models/AcousticModel.py
+++ models/AcousticModel.py
@@ -1014,7 +1014,7 @@ class AcousticModel(object):
                 _ = self._write_timeline(run_metadata, inter_time, "end_batch")
 
             # Step result
-            client.update_task_info({'loss':float(mean_loss),'error_rate': float(mean_error_rate),'time': time.time() - start_time})
+            client.update_task_info({'loss':float(mean_loss),'error_rate': float(mean_error_rate),'time': int(time.time() - start_time)})
             logging.info("Batch %d : loss %.5f - error_rate %.5f - duration %.2f",
                          current_step, mean_loss, mean_error_rate, time.time() - start_time)
 
